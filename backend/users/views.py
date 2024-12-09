f4c1bad75fed1afae34701cab9f7e2ba08957a76
Ketaki-Tam <tamketaki@gmail.com>
2024-12-10 00:45:14 +0530

    Added comments

--- backend/users/views.py
+++ backend/users/views.py
@@ -7,7 +7,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 import json
 
-
+#function to register as a user
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
